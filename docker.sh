0e5da656081104ae02d6cd509e554e61ba0c3475
Nythique <nythique@proton.me>
2025-06-24 23:19:24 +0000

    Installer NewVMs

--- docker.sh
+++ docker.sh
@@ -10,21 +10,21 @@ sudo apt install -y \
     curl \
     gnupg \
     lsb-release
-cls
+clear
 echo "==================🔐 Ajout de la clé GPG Docker...=================="
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
     sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-cls
+clear
 echo "==================📦 Ajout du dépôt Docker à APT...=================="
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-cls
+clear
 echo "==================🔄 Mise à jour des paquets avec dépôt Docker...=================="
 sudo apt update
-cls
+clear
 echo "==================🐳 Installation de Docker Engine...=================="
 sudo apt install -y \
     docker-ce \
@@ -32,10 +32,10 @@ sudo apt install -y \
     containerd.io \
     docker-buildx-plugin \
     docker-compose-plugin
-cls
+clear
 echo "==================👤 Ajout de l'utilisateur $USER au groupe docker...=================="
 sudo usermod -aG docker $USER
-cls
+clear
 echo "==================✅ Docker est installé. Pour lancer le daemon :=================="
 echo "    sudo dockerd &"
 echo "Et ensuite, dans un autre terminal :"
