8bbf0b7ec829e2c4449353b401b10fb9855fffcb
Nythique <nythique@proton.me>
2025-07-15 22:05:08 +0000

    Build in Public ----> Next update

---
+++ discord/ext/recorder/__init__.py
@@ -0,0 +1,18 @@
+from .voice_client import *
+from .reader import *
+from .sinks import *
+from .video import *
+from .opus import *
+from .rtp import *
+from .enums import *
+
+from . import (
+    rtp as rtp,
+    extras as extras,
+)
+
+__title__ = 'discord.ext.recover'
+__author__ = 'Nythique'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present Nythique'
+__version__ = '0.0.1'
