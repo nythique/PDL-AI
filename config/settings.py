c1f3d9faed2f66dd3af3d06662b023d18bcb64ae
Nythique <nythique@proton.me>
2025-07-21 21:00:28 +0000

    update prompt manager

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ import os
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN") 
 NAME_IA = ("pdl", "PDL", "Pdl", "pDL") 
 VERSION = os.getenv("VERSION") 
-PREFIX = os.getenv("PREFIX", "+")  
+PREFIX = os.getenv("PREFIX")  
 
 # ========================== CHEMINS DES FICHIERS ==========================
 ERROR_LOG_PATH = "logs/error/error.log" 
@@ -20,25 +20,25 @@ MEMORY_MAX_INACTIVE_TIME = 5 # en minutes
 MEMORY_CLEAR_TIME  = 1440 # en minutes
 
 # NOTE:========================== PARAMÈTRES DE STYLE ==========================
-TYPING_TIME = 0.1 
+TYPING_TIME = 0.2 
 STATUS_TIME = 3  
 SLOWTYPE_TIME = 0.1  
 
 # NOTE:========================== IDENTIFIANTS ET NOMS ==========================
-ALERT_CHANNEL = os.getenv("ALERT_CHANNEL")
+ALERT_CHANNEL = 1396440471881908426
 ROOT_USER = (969287987672268840, 767678057770385438, 1233020939898327092, 679664711788396552) 
 
 #  NOTE:========================== PARAMÈTRES DE L'IA CHAT ==========================
 CHAT_KEY = os.getenv("GROQ_API_KEY") 
-CHAT_MODEL = "llama3-70b-8192"  
-CHAT_FREQUENCY = 1  
-CHAT_TEMPERATURE = 0 
-CHAT_MAX_TOKENS = 512  
-CHAT_TOP_P = 0.95  
-CHAT_PRESENCE_PENALTY = 0  #
+CHAT_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct" 
+CHAT_TEMPERATURE = 0.85  
+CHAT_FREQUENCY = 0.2 
+CHAT_PRESENCE_PENALTY = 0.2
+CHAT_TOP_P = 0.8
+CHAT_MAX_TOKENS = 1024  
 CHAT_STOP = ["@"]  
 # Limite des messgaes en contexte
-CHAT_LIMIT_MEMORY = 5 
+CHAT_LIMIT_MEMORY = 10 
 
 #  NOTE:========================== PROMPTS DE L'IA ==========================
 from archive.meta import prompt
