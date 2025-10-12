aa933e9f675923ac158c1305fbae264d76a63dda
Nythique <nythique@proton.me>
2025-10-12 16:14:22 +0000

    Correction de on_message (channelData)

--- home/core/main.py
+++ home/core/main.py
@@ -254,7 +254,8 @@ def registerCommands(botInstance):
     async def on_message(message):
         db.loadData() 
         if message.author.bot: return 
-        if message.channel.id not in db.selectData("channelList"): return
+        channelData = db.selectData("channelList") or []
+        if not channelData: return
         if any(key in message.content for key in UNAUTHO_WORDS):
             await message.channel.send(f"Je ne peux pas te répondre. Tu as utilisé une mauvaise expression.")
             return
